f785f8e7d46b80f0e24defb35d71e553f06d94a5
william cardoso <williancard123@gmail.com>
2024-10-18 20:05:50 -0400

    doc: fix doc auth ticket

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Support')
+@ApiBearerAuth('JWT-auth')
 @Controller('support')
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
